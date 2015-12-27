55bca085e295632b097d9e6f6442ecd107f64ed3
mattfxyz <matt.faluotico@gmail.com>
2015-12-26 19:48:40 -0500

    listeners for events

--- SpotifyTodayWidget/TodayViewController.swift
+++ SpotifyTodayWidget/TodayViewController.swift
@@ -12,13 +12,14 @@ import NotificationCenter
 class TodayViewController: NSViewController, NCWidgetProviding {
 
     var centerReceiver = NSDistributedNotificationCenter()
+    var isPlaying = true;
     
     override var nibName: String? {
         return "TodayViewController"
     }
     
     override func viewDidAppear() {
-
+        common();
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
@@ -26,6 +27,11 @@ class TodayViewController: NSViewController, NCWidgetProviding {
         // with NoData if nothing has changed or NewData if there is new data since the last
         // time we called you
         completionHandler(.NoData)
+    }
+    
+    
+    func common() {
+        
     }
     
     // MARK: Track info labsl
@@ -44,18 +50,37 @@ class TodayViewController: NSViewController, NCWidgetProviding {
     // MARK: Button Actions
     
     @IBAction func shareButton(sender: AnyObject) {
+        self.centerReceiver.postNotificationName("SpotifyToday", object: "share", userInfo: nil);
     }
     
     @IBAction func nextButton(sender: AnyObject) {
+        self.centerReceiver.postNotificationName("SpotifyToday", object: "next", userInfo: nil);
     }
 
     @IBAction func playButton(sender: AnyObject) {
+        self.centerReceiver.postNotificationName("SpotifyToday", object: "toggle", userInfo: nil);
     }
     
     @IBAction func previousButton(sender: AnyObject) {
+        self.centerReceiver.postNotificationName("SpotifyToday", object: "previous", userInfo: nil);
     }
     
     @IBAction func addButton(sender: AnyObject) {
+        self.centerReceiver.postNotificationName("SpotifyToday", object: "add", userInfo: nil);
+    }
+    
+    
+    func togglePlay() {
+        self.playPauseButton.image = isPlaying ? NSImage(named: "pause") : NSImage(named: "play");
+    }
+    
+    func update() {
+        let defaults = NSUserDefaults(suiteName: "mpf.SpotifyToday.widget");
+        if let data = defaults {
+            self.songLabel.stringValue = data.stringForKey("song") ?? "song";
+            self.artistLabel.stringValue = data.stringForKey("artist") ?? "artst";
+            self.albumLabel.stringValue = data.stringForKey("album") ?? "album";
+        }
     }
     
 }
